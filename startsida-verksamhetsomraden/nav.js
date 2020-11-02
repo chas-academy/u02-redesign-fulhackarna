8df8e6ed2e6d3f80ac59f26afe919667159b9459
Talica2005 <talica2005@gmail.com>
2020-11-02 18:20:54 +0100

    add start and verksamhetsomraden page

---
+++ startsida-verksamhetsomraden/nav.js
@@ -0,0 +1,28 @@
+// Search Icon click
+document.querySelector(".searchIcon").addEventListener("click", function(){
+    document.querySelector("input").classList.toggle("searchActive");
+})
+
+//Scroll
+const nav = document.querySelector(".normalNav");
+
+window.onscroll= function(){
+    let top = window.scrollY;
+    console.log(top);
+    if (top >= 10){
+        nav.classList.add("onScroll")
+    }else{
+        nav.classList.remove("onScroll");
+    }
+} 
+
+//Responsive
+
+const hamburger = document.getElementById("hamburger");
+const nav2 = document.getElementById("nav_options");
+
+hamburger.addEventListener("click", () => {
+    nav2.classList.toggle("responsiveShow");
+});
+
+
